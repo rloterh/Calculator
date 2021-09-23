c6d70d5c3582ad43f6c62780a3b1e2d52d5b4573
rloterh <rloterh@gmail.com>
2021-09-23 13:52:48 +0000

    Add Display and ButtonPanel componts to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
+import ButtonPanel from './ButtonPanel';
+import Display from './Display';
+
 function App() {
   return (
     <div className="App">
+      <Display />
+      <ButtonPanel />
     </div>
   );
 }
