b8a7177bd077b168ef62b91f8826f286ece520c5
rloterh <rloterh@gmail.com>
2021-09-23 13:20:35 +0000

    Add Button component and a label prop

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+const Button = ({ label }) => (
+  <div>
+    {label}
+  </div>
+);
+
+Button.propTypes = {
+  label: PropTypes.string.isRequired,
+};
+
+export default Button;
