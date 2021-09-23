3e8cf1d3054cda9a23127b020033099216d1d1ea
rloterh <rloterh@gmail.com>
2021-09-23 13:22:09 +0000

    Add ButtonPanel component with Button components

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,2 +1,38 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import Button from './Button';
+
+const ButtonPanel = () => (
+  <div>
+    <div>
+      <Button label="AC" />
+      <Button label="+/-" />
+      <Button label="%" />
+      <Button label="/" />
+    </div>
+    <div>
+      <Button label="7" />
+      <Button label="8" />
+      <Button label="9" />
+      <Button label="X" />
+    </div>
+    <div>
+      <Button label="4" />
+      <Button label="5" />
+      <Button label="6" />
+      <Button label="-" />
+    </div>
+    <div>
+      <Button label="1" />
+      <Button label="2" />
+      <Button label="3" />
+      <Button label="+" />
+    </div>
+    <div>
+      <Button label="0" />
+      <Button label="." />
+      <Button label="=" />
+    </div>
+  </div>
+);
+
+export default ButtonPanel;
