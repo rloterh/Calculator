26e202ad83cc42cce60be10ddd057f3e9fcff8ce
rloterh <rloterh@gmail.com>
2021-10-10 12:45:43 +0000

    Add styling to display

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Display = ({ result }) => (
-  <>
+  <div className="display">
     {result}
-  </>
+  </div>
 );
 
 Display.propTypes = {
