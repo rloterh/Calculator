8e2e942d75141c12885bde43272d0d930d61e552
rloterh <rloterh@gmail.com>
2021-10-17 22:07:54 +0000

    Update main component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,35 @@
+import React from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
+import calculate from '../logic/calculate';
 
-const App = () => (
-  <div className="App">
-    <Display />
-    <ButtonPanel />
-  </div>
-);
+const App = () => {
+  React.state = {
+    total: null,
+    next: null,
+    operation: null,
+  };
+
+  const handleClick = (buttonName) => {
+    const newState = calculate(React.state, buttonName);
+    React.setState(newState);
+  };
+
+  const update = () => {
+    const { total, next, operation } = React.state;
+
+    if (next) {
+      return next;
+    }
+    return total;
+  };
+
+  return (
+    <div className="App">
+      <Display result={update()} />
+      <ButtonPanel handleClick={handleClick} />
+    </div>
+  );
+};
 
 export default App;
