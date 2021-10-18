67267ae6e0906e8379101ae1a521620a297dccc0
rloterh <rloterh@gmail.com>
2021-10-18 12:27:23 +0000

    Update main component to a stateful component

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 import calculate from '../logic/calculate';
 
-const App = () => {
-  React.state = {
-    total: null,
-    next: null,
-    operation: null,
-  };
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-  const handleClick = (buttonName) => {
-    const newState = calculate(React.state, buttonName);
-    React.setState(newState);
-  };
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
 
-  const update = () => {
-    const { total, next, operation } = React.state;
+  handleClick = (buttonName) => {
+    const newState = calculate(this.state, buttonName);
+    this.setState(newState);
+  }
 
+  update() {
+    const { total, next, operation } = this.state;
     if (next) {
       return next;
     }
     return total;
-  };
+  }
 
-  return (
-    <div className="App">
-      <Display result={update()} />
-      <ButtonPanel handleClick={handleClick} />
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="App">
+        <Display result={this.update()} />
+        <ButtonPanel handleClick={this.handleClick} />
+      </div>
+    );
+  }
+}
 
 export default App;
